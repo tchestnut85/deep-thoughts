49214ba4c5149593a7570600d9d2df0677d197d1
tchestnut85 <tchestnut85@gmail.com>
2021-01-18 10:04:52 -0500

    updated thought forms

---
+++ client/src/components/ReactionForm/index.js
@@ -0,0 +1,52 @@
+import React, { useState } from 'react';
+import { useMutation } from '@apollo/react-hooks';
+import { ADD_REACTION } from '../../utils/mutations';
+
+const ReactionForm = ({ thoughtId }) => {
+    const [reactionBody, setBody] = useState('');
+    const [characterCount, setCharacterCount] = useState(0);
+    const [addReaction, { error }] = useMutation(ADD_REACTION);
+
+    const handleChange = event => {
+        if (event.target.value.length <= 280) {
+            setBody(event.target.value);
+            setCharacterCount(event.target.value.length);
+        }
+    };
+
+    const handleFormSubmit = async event => {
+        event.preventDefault();
+
+        try {
+            await addReaction({
+                variables: { reactionBody, thoughtId }
+            });
+            setBody('');
+            setCharacterCount(0);
+        } catch (e) {
+            console.error(e);
+        }
+    };
+
+    return (
+        <div>
+            <p className="m-0">
+                Character Count: {characterCount}/280
+            </p>
+            <form onChange={handleFormSubmit} className="flex-row justify-center justify-space-between-md align-stretch">
+                <textarea
+                    onChange={handleChange}
+                    placeholder="Leave a reaction to this thought..."
+                    className="form-input col-12 col-md-9"
+                >{reactionBody}</textarea>
+
+                <button className="btn col-12 col-md-3" type="submit">
+                    Submit
+                </button>
+            </form>
+            {error && <span className="ml-2">Something went wrong...</span>}
+        </div>
+    );
+};
+
+export default ReactionForm;
--- client/src/components/ThoughtForm/index.js
+++ client/src/components/ThoughtForm/index.js
@@ -1,8 +1,37 @@
 import React, { useState } from 'react';
+import { useMutation } from '@apollo/react-hooks';
+import { ADD_THOUGHT } from '../../utils/mutations';
+import { QUERY_THOUGHTS, QUERY_ME } from '../../utils/queries';
 
 const ThoughtForm = () => {
     const [thoughtText, setText] = useState('');
     const [characterCount, setCharacterCount] = useState(0);
+
+    // add the new thought to existing thoughts array
+    const [addThought, { error }] = useMutation(ADD_THOUGHT, {
+        update(cache, { data: { addThought } }) {
+            try {
+
+                // read whats currently in the cache
+                const { thoughts } = cache.readQuery({ query: QUERY_THOUGHTS });
+
+                // PREPEND the newest thought to the front of the array
+                cache.writeQuery({
+                    query: QUERY_THOUGHTS,
+                    data: { thoughts: [addThought, ...thoughts] }
+                });
+            } catch (e) {
+                console.error(e);
+            }
+
+            // update the object's cache, appending new thought to the end of the array
+            const { me } = cache.readQuery({ query: QUERY_ME });
+            cache.writeQuery({
+                query: QUERY_ME,
+                data: { me: { ...me, thoughts: [...me.thoughts, addThought] } }
+            });
+        }
+    });
 
     const handleChange = event => {
         if (event.target.value.length <= 280) {
@@ -13,14 +42,27 @@ const ThoughtForm = () => {
 
     const handleFormSubmit = async event => {
         event.preventDefault();
-        setText('');
-        setCharacterCount(0);
+
+        try {
+            // add thought to database
+            await addThought({
+                variables: { thoughtText }
+            });
+
+            // clear the form value
+            setText('');
+            setCharacterCount(0);
+        } catch (e) {
+            console.error(e);
+        }
+
     };
 
     return (
         <div>
-            <p className={`m-0 ${characterCount === 280 ? 'Text limit reached.' : ''}`}>
+            <p className={`m-0 ${characterCount === 280 || error ? 'Text limit reached.' : ''}`}>
                 Character Count: {characterCount}/280
+                {error && <span className='ml-2'>Something went wrong...</span>}
             </p>
             <form className="flex-row justify-center justify-space-between-md align-stretch"
                 onSubmit={handleFormSubmit}
